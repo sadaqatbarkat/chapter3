ba7505b66074a50c7f810b58bba39c049a00db6a
sadaqatbarkat <153889463+sadaqatbarkat@users.noreply.github.com>
2024-03-15 16:44:30 +0500

    add index and app.js files

---
+++ app.js
@@ -0,0 +1,24 @@
+//var age ="Iam 18 years old"
+//alert(age)
+
+
+
+
+//var visitorTime="You have visited this site 14 times"
+//alert(visitorTime)
+
+
+
+
+
+//document.write("<p>My brith year is 1990 </p> ")
+
+
+//document.write( " <p>Data type of my ddeclared variable is number</p>")
+
+
+//var clothing="<strong>Sadaqat Ali <span> orderd </span> 4 Sindhi cloth Mujeeb Store.</strong>"
+ //document.write(clothing)
+
+
+ 
